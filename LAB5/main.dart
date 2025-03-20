0ad0b0fb05cad3c07f2658e802878da551358f9d
praveenkumar6711 <praveenmaturi03@gmail.com>
2025-03-20 13:07:24 +0530

    Add files via upload

--- LAB5/main.dart
+++ LAB5/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LApp());
+}
+class LApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(color:Colors.cyanAccent,child:Text('FIRST')),
+              Container(color:Colors.lightGreenAccent, child:Text('SECOND')),
+              Container(color:Colors.pink, child:Text('THIRD')),
+              Container(color:Colors.limeAccent, child:Text('FOURTH')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
